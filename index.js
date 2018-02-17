f870f10755d5245406e27194804b372a7395c2df
vineet <vineet.12cs069@abes.ac.in>
2018-02-17 10:09:57 +0530

    console page entry

--- index.js
+++ index.js
@@ -34,6 +34,39 @@ app.get('/webhook', (req, res) => {
     }
 })
 
-app.get('/hello', function(req, res){
-   res.send("World"); 
-})
+
+app.post('/webhook', function (req, res) {
+  var data = req.body;
+
+  // Make sure this is a page subscription
+  if (data.object == 'page') {
+    // Iterate over each entry
+    // There may be multiple if batched
+    data.entry.forEach(function(pageEntry) {
+      var pageID = pageEntry.id;
+      var timeOfEvent = pageEntry.time;
+
+      // Iterate over each messaging event
+      console.log("pageEntry :",pageEntry);
+      pageEntry.messaging.forEach(function(messagingEvent) {
+
+          console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+        
+      });
+    });
+
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know you've
+    // successfully received the callback. Otherwise, the request will time out.
+    res.sendStatus(200);
+  }
+});
+
+/*
+DB 
+whenver webhoook invoke save or delete user comment
+
+page load -> fetch data from db
+
+*/
